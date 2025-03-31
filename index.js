fc3fc16a310531c2cca4b5eb6485964e6ab6f84e
aditya dey <deyaditya34@gmail.com>
2025-03-31 21:22:38 +0530

    redesigned the player move

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 const player = {
-    color: "red",
-    position: 0
+  color: 'red',
+  position: 0,
 };
 
+function movePlayer(steps) {
+  let playerPosition = player.position;
+  
+  playerPosition += steps;
+
+  if (playerPosition > 51) {
+    const excessSteps =  playerPosition - 51;
+    playerPosition = 0;
+    playerPosition += excessSteps
+  }
+
+  player.position = playerPosition;
+  console.log(playerPosition)
+}
+
+movePlayer(10)
+movePlayer(40)
+movePlayer(5)
